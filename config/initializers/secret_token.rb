605ba80994bb54b1d1e7c01e237482e90e530d79
luciferatos <luciferatos@luciferatos-workstation.(none)>
2014-01-20 13:34:39 -0200

    initial commit, app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImaBoldTest::Application.config.secret_key_base = '26421a34b93e97912ce1aa483d8d661874966b8bdc61ca564ddca9000c886ac0ac3d101d58f6180509bf133436cda7f16ac412a5ab2f5470ba073b111d368fe1'

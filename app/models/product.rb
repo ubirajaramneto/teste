a9718a0c46a80e8fcfe61981917672b4a5b9cbfe
luciferatos <luciferatos@luciferatos-workstation.(none)>
2014-01-20 16:13:28 -0200

    completed model validations and tests for both category and products table

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
 	#=======VALIDATIONS========
 
 	validates_associated :category
-
+	validates :category, :presence => true
 	validates :name, :length => {maximum: 30}, :presence => true
 	validates :price, :numericality => {:greater_than => 0}, :presence => true
 	validates :active, :presence => true
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "tests presence validation for all object fields" do
+		product = Product.new(:name => "", :price => "gh", :active => "", :category_id => "")
+		expect(product).to have(1).error_on(:name)
+		expect(product).to have(1).error_on(:price)
+		expect(product).to have(1).error_on(:active)
+	end
+
+	it "tests the numeracality of the price field" do
+		product = Product.new(:name => "chocolate", :price => 0.00, :active => true, :category_id => 2)
+		expect(product).to have(1).error_on(:price)
+	end
+
+	it "tests the association of the product in a category" do
+		Product.create(:name => "chocolate", :price => 2.00, :active => true, :category_id => nil)
+		expect(Product).to have(0).record
+	end
+
 end

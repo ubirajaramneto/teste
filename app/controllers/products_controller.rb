6f632b6bd4f45da3b2eaba3699565d965edf4a1a
luciferatos <luciferatos@luciferatos-workstation.(none)>
2014-01-20 17:26:31 -0200

    updated routes.rb and created controllers actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,40 @@
 class ProductsController < ApplicationController
+
+	def index
+		@products_index = Product.all
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def destroy
+	end
+
+	def destroy_selected
+	end
+
+	def activate
+	end
+
+	def deactivate
+	end
+
+	def activate_selected
+	end
+
+	def deactivate_selected
+	end
+
 end
